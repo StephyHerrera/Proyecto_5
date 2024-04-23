846aa1c712dce9ab2823dcaff6643c508cd01fb0
Stephy Herrera <155865616+StephyHerrera@users.noreply.github.com>
2024-04-23 01:20:27 +0000

    create my first python script

--- app.py
+++ app.py
@@ -16,3 +16,18 @@ fig = px.histogram(car_data, x="odometer")
         
    # mostrar un gráfico Plotly interactivo
 st.plotly_chart(fig, use_container_width=True)
+
+st.header('Condición de los Vehículos')
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir scatter') 
+ 
+
+if hist_button: # al hacer clic en el botón
+ # escribir un mensaje
+ st.write('Creación de un scatter para el conjunto de datos de anuncios de venta de coches')
+            
+   # crear un histograma
+fig = px.scatter(car_data, x="odometer", y="price")
+st.plotly_chart(fig, use_container_width=True)
+        
